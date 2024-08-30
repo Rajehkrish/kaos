81a2385ba20ae62285bbd38e173798cece09eb3a
root <root@ip-172-31-36-201.ap-south-1.compute.internal>
2024-08-30 18:42:34 +0000

    initial commit

--- app.py
+++ app.py
@@ -10,3 +10,4 @@ def get_public_ip():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,9 @@
+import os
+import sys
+
+# Add the project root directory to the sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app import app
 
 def test_index():
@@ -5,3 +11,4 @@ def test_index():
     response = client.get('/')
     assert response.status_code == 200
     assert 'Your public IP address is:' in response.data.decode()
+
